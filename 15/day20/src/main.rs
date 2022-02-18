fce76700315e73a88dc5b3d3015d27058e1ed238
Adrian Hornung <hornung.adrian@gmail.com>
2022-02-18 13:30:13 +0100

    day20

--- 15/day20/src/main.rs
+++ 15/day20/src/main.rs
@@ -1,73 +1,53 @@
-const NEEDED: u64 = 36000000;
+#![allow(dead_code)]
+const NEEDED: usize = 36000000;
 
 fn main() {
-    println!("result <{}>", one());
+    println!("result <{}>", two());
 }
 
-fn one() -> u64 {
-    one_inner(NEEDED)
-}
+fn one() -> usize {
+    let n = NEEDED;
+    let mut houses = vec![0; n / 10];
 
-fn one_inner(needed: u64) -> u64 {
-    // search upper => more then exp
-    let mut count = 1;
-    let mut house = 1;
-    while count < needed {
-        house *= 2; // make large steps
-        count = get_house_presents(house);
+    // elves
+    for i in 1..(n / 10) {
+        // houses
+        for j in (i..(n / 10)).step_by(i) {
+            houses[j] += i * 10;
+        }
     }
 
-    println!("upper house <{}>", house);
+    houses
+        .iter()
+        .enumerate()
+        .filter(|(_, v)| **v >= n)
+        .map(|(i, _)| i)
+        .next()
+        .unwrap()
+}
 
-    // find exact via binanry search approach
-    // if mid is too low then upper is still house
-    // else mid will be upper repeate until mid
-    // and low are equal or low is larger then mid
-    let mut top = house;
-    let mut low = house / 2;
-    let mut last = None;
-    while low <= top {
-        let mid = top - (top - low) / 2;
-        let curr = get_house_presents(mid);
+fn two() -> usize {
+    let n = NEEDED / 10;
+    let mut houses = vec![0; n];
 
-        if let Some(v) = last {
-            if v == curr {
+    // elves
+    for i in 1..n {
+        let mut visited = 0;
+        // houses
+        for j in (i..n).step_by(i) {
+            if visited >= 50 {
                 break;
             }
-        }
-
-        last = Some(curr);
-
-        if curr < needed {
-            low = mid;
-        } else if curr > needed {
-            top = mid;
-        } else {
-            // we found it
-            return mid;
-        }
-
-        println!(
-            "current: <{curr}>, state: <{}>, top: {top}, mid: {mid}, low: {low}",
-            curr < needed
-        );
-    }
-
-    top
-}
-
-fn get_house_presents(house: u64) -> u64 {
-    let mut current = 0;
-    for elf_nr in 1..=house {
-        if house % elf_nr == 0 {
-            current += elf_nr * 10;
+            houses[j] += i * 11;
+            visited += 1;
         }
     }
-    current
-}
 
-#[test]
-fn test_house() {
-    assert_eq!(150, get_house_presents(8));
-    assert_eq!(130, get_house_presents(9));
+    houses
+        .iter()
+        .enumerate()
+        .filter(|(_, v)| **v >= NEEDED)
+        .map(|(i, _)| i)
+        .next()
+        .unwrap()
 }
