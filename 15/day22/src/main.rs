6162798be7f066605e55be39d95263bd9f5f8daa
Adrian Hornung <hornung.adrian@gmail.com>
2022-02-20 21:07:30 +0100

    day22

--- 15/day22/src/main.rs
+++ 15/day22/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use std::collections::VecDeque;
+
 use strum::{EnumCount, EnumIter, FromRepr, IntoEnumIterator};
 
 const INPUT: &str = include_str!("../input.txt");
@@ -11,16 +13,84 @@ const ME: Hero = Hero {
 };
 
 fn main() {
-    println!("result <{}>", one());
+    println!("result <{}>", two());
 }
 
 fn one() -> Mana {
     let boss = input().unwrap();
-    one_inner(ME, boss)
+    battle(ME, boss, false)
 }
 
-fn one_inner(hero: Hero, boss: Boss) -> Mana {
-    minimax(hero, boss, 0, ActiveEffects::default()).unwrap()
+fn two() -> Mana {
+    let boss = input().unwrap();
+    battle(ME, boss, true)
+}
+
+fn battle(hero: Hero, boss: Boss, hard: bool) -> Mana {
+    let mut min_mana = Mana::MAX;
+    let mut queue = VecDeque::with_capacity(Attacks::COUNT * 4);
+
+    for attack in Attacks::iter() {
+        queue.push_back(State {
+            hero: hero.clone(),
+            boss: boss.clone(),
+            attack,
+            active: ActiveEffects::default(),
+        });
+    }
+
+    while let Some(mut state) = queue.pop_back() {
+        if hard {
+            state.hero.hit_points = state.hero.hit_points.saturating_sub(1);
+            if state.hero.hit_points == 0 {
+                continue;
+            }
+        }
+
+        //// HERO's turn
+        if let Some(mana_used) = pre_effects(&mut state.hero, &mut state.boss, &mut state.active) {
+            min_mana = std::cmp::min(min_mana, mana_used);
+            continue;
+        }
+
+        // check if the hero's attack has killed the boss
+        if hero_attack(&mut state) {
+            min_mana = std::cmp::min(min_mana, state.hero.used_mana);
+            continue;
+        }
+
+        //// BOSS's turn
+        if let Some(mana_used) = pre_effects(&mut state.hero, &mut state.boss, &mut state.active) {
+            min_mana = std::cmp::min(min_mana, mana_used);
+            continue;
+        }
+
+        // check if boss's attack has killed the hero
+        if boss_attack(&mut state.hero, &state.boss) {
+            continue;
+        }
+
+        // hero prepares for next attack
+        for attack in Attacks::iter() {
+            // check that the hero has the mana needed to use the spell
+            if state.hero.mana <= attack.mana() {
+                continue;
+            }
+            // check that the next attack will not be more expensive
+            // then the current best
+            if state.hero.used_mana + attack.mana() >= min_mana {
+                continue;
+            }
+
+            queue.push_back(State {
+                hero: state.hero.clone(),
+                boss: state.boss.clone(),
+                attack,
+                active: state.active.clone(),
+            });
+        }
+    }
+    min_mana
 }
 
 fn pre_effects(hero: &mut Hero, boss: &mut Boss, active: &mut ActiveEffects) -> Option<Mana> {
@@ -57,114 +127,59 @@ fn pre_effects(hero: &mut Hero, boss: &mut Boss, active: &mut ActiveEffects) -> 
     None
 }
 
-fn minimax(mut hero: Hero, mut boss: Boss, depth: u16, mut active: ActiveEffects) -> Option<Mana> {
-    // run preround effects / and check for a win
-    if let Some(mana) = pre_effects(&mut hero, &mut boss, &mut active) {
-        return Some(mana);
-    }
-
-    if depth % 2 == 0 {
-        hero_attack(hero, boss, depth + 1, active)
-    } else {
-        boss_attack(hero, boss, depth + 1, active)
-    }
-}
-
-fn boss_attack(mut hero: Hero, boss: Boss, depth: u16, active: ActiveEffects) -> Option<Mana> {
+fn boss_attack(hero: &mut Hero, boss: &Boss) -> bool {
     // boss
     hero.hit_points = hero
         .hit_points
         .saturating_sub(std::cmp::max(boss.damage.saturating_sub(hero.armor), 1));
 
-    // this is a loosing path
-    if hero.hit_points == 0 {
-        None
-    } else {
-        // simple recursion no branches needed for boss move
-        minimax(hero, boss, depth, active)
-    }
+    // is this a loosing path?
+    hero.hit_points == 0
 }
 
-fn hero_attack(hero: Hero, boss: Boss, depth: u16, active: ActiveEffects) -> Option<Mana> {
-    // mine
-    let mut prevs = None;
+fn hero_attack(state: &mut State) -> bool {
+    let (id, att, cost, durr) = (
+        state.attack as usize,
+        state.attack,
+        state.attack.mana(),
+        state.attack.duration(),
+    );
 
-    for (id, att, cost, durr) in Attacks::iter()
-        .map(|att| (att as usize, att, att.mana(), att.duration()))
-        // check if effect is active => don't try to use it again
-        // until after the cool down
-        .filter(|(id, _, _, _)| active[*id] == 0)
-    {
-        let mut hero = hero.clone();
-        let mut boss = boss.clone();
-        let mut active = active.clone();
+    // set the duration of the effect
+    state.active[id] = durr;
 
-        // set the duration of the effect
-        active[id] = durr;
+    // make sure that the hero has the mana needed for this
+    // it should already be checked, but checking it again
+    // should not be an issue
+    assert!(
+        state.hero.mana >= cost,
+        "hero's mana has run out during his attack"
+    );
 
-        // check if there is enough mana for effect
-        if cost > hero.mana {
-            // no path
-            continue;
+    state.hero.mana -= cost;
+    state.hero.used_mana += cost;
+
+    let mut do_damage = |damage| {
+        // do the damage
+        state.boss.hit_points = state.boss.hit_points.saturating_sub(damage);
+        state.boss.hit_points == 0
+    };
+
+    match att {
+        Attacks::MagicMissile => do_damage(4),
+        Attacks::Drain => {
+            state.hero.hit_points += 2;
+            do_damage(2)
         }
-
-        hero.mana -= cost;
-        hero.used_mana += cost;
-
-        // check if new path will be cheaper then previous winning one
-        // (pruning step)
-        if let Some(other_mana) = prevs {
-            if other_mana > hero.used_mana {
-                continue;
-            }
+        Attacks::Shield => {
+            state.hero.armor += 7;
+            false
         }
-
-        let mut check_win = |mana| {
-            // found a winning path
-            // check if previous win exists and if
-            // it was better or worse
-            prevs = Some(prevs.map_or(mana, |other| std::cmp::min(other, mana)));
-            eprintln!("found winning path <{:?}>", mana);
-        };
-
-        let mut do_damage = |damage| {
-            // do the damage
-            boss.hit_points = boss.hit_points.saturating_sub(damage);
-            if boss.hit_points == 0 {
-                // winning path, so check if better score
-                check_win(hero.used_mana);
-                true
-            } else {
-                false
-            }
-        };
-
-        match att {
-            Attacks::MagicMissile => {
-                if do_damage(4) {
-                    continue;
-                }
-            }
-            Attacks::Drain => {
-                if do_damage(2) {
-                    continue;
-                }
-                hero.hit_points += 2;
-            }
-            Attacks::Shield => {
-                hero.armor += 7;
-            }
-            Attacks::Poison | Attacks::Recharge => {
-                // no direct effect
-            }
-        }
-
-        // try to follow this path to the end
-        if let Some(res) = minimax(hero, boss, depth, active) {
-            check_win(res);
+        Attacks::Poison | Attacks::Recharge => {
+            // no direct effect
+            false
         }
     }
-    prevs
 }
 
 type Mana = u16;
@@ -204,10 +219,18 @@ impl Attacks {
 }
 
 #[derive(Debug, Clone)]
+pub struct State {
+    hero: Hero,
+    boss: Boss,
+    active: ActiveEffects,
+    attack: Attacks,
+}
+
+#[derive(Debug, Clone)]
 pub struct Hero {
     pub hit_points: u8,
     pub armor: u8,
-    pub mana: u16,
+    pub mana: Mana,
     pub used_mana: Mana,
 }
 
