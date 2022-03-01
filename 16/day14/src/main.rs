595aea62ccc1e9eee1d349e17e330d4dc49fd3f6
Adrian Hornung <hornung.adrian@gmail.com>
2022-03-01 12:29:11 +0100

    initial day14.2

--- 16/day14/src/main.rs
+++ 16/day14/src/main.rs
@@ -1,9 +1,11 @@
+#![allow(dead_code)]
+
 use std::{collections::VecDeque, fmt::Write};
 
 const INPUT: &str = "ahsbgdzn";
 
 fn main() {
-    println!("result {}", one());
+    println!("result {}", two());
 }
 
 fn one() -> usize {
@@ -14,20 +16,6 @@ struct KeyGen<'input> {
     idx: usize,
     salt: &'input str,
     buffer: String,
-}
-
-impl KeyGen<'_> {
-    fn next(&mut self) -> Key {
-        loop {
-            // check until we get a potential start / end
-            self.buffer.clear();
-            let key = Key::with_buffer(self.idx, self.salt, &mut self.buffer);
-            self.idx += 1;
-            if let Some(key) = key {
-                break key;
-            }
-        }
-    }
 }
 
 fn one_inner(input: &str) -> usize {
@@ -64,7 +52,7 @@ fn one_inner(input: &str) -> usize {
                     queue.push_back(next);
                 }
                 if has {
-                    print!(" 0 {} -- {}", key_gen.idx, queue.len());
+                    print!(" O {} -- {}", key_gen.idx, queue.len());
                     count += 1;
                 }
 
@@ -77,6 +65,88 @@ fn one_inner(input: &str) -> usize {
             _ => {
                 queue.push_back(key_gen.next());
                 continue;
+            }
+        }
+    }
+}
+
+fn two() -> usize {
+    two_inner(INPUT)
+}
+
+fn two_inner(input: &str) -> usize {
+    const QUEUE_SIZE: usize = 1000;
+    let mut queue = VecDeque::with_capacity(QUEUE_SIZE);
+    let mut key_gen = KeyGen {
+        idx: 0,
+        salt: input,
+        buffer: String::with_capacity(100),
+    };
+
+    let mut count = 0;
+
+    loop {
+        match queue.pop_front() {
+            Some(ref a @ Key { three: Some(_), .. }) => {
+                print!("count {count} - {:?}", a);
+                // search in list for next possible 5
+                let mut it = queue.iter();
+                let mut has = false;
+
+                while let Some(v) = it.next() {
+                    if a.pair(v) {
+                        has = true;
+                        break;
+                    }
+                }
+
+                // if there where not enough keys calculated
+                while queue.len() <= QUEUE_SIZE && !has {
+                    let next = key_gen.next_stretched();
+                    has = a.pair(&next);
+
+                    queue.push_back(next);
+                }
+                if has {
+                    print!(" O {} -- {}", key_gen.idx, queue.len());
+                    count += 1;
+                }
+
+                println!();
+
+                if count >= 64 {
+                    break a.idx;
+                }
+            }
+            _ => {
+                queue.push_back(key_gen.next_stretched());
+                continue;
+            }
+        }
+    }
+}
+
+impl KeyGen<'_> {
+    fn next(&mut self) -> Key {
+        loop {
+            // check until we get a potential start / end
+            self.buffer.clear();
+            let key = Key::with_buffer(self.idx, self.salt, &mut self.buffer);
+            self.idx += 1;
+            if let Some(key) = key {
+                break key;
+            }
+        }
+    }
+
+    fn next_stretched(&mut self) -> Key {
+        loop {
+            // check until we get a potential start / end
+            self.buffer.clear();
+            let key = Key::with_buffer_stretched(self.idx, self.salt, &mut self.buffer);
+            self.idx += 1;
+            if let Some(key) = key {
+                break key;
             }
         }
     }
@@ -95,9 +165,28 @@ struct Key {
 
 impl Key {
     fn with_buffer(idx: usize, input: &str, mut tmp: &mut String) -> Option<Self> {
+        tmp.clear();
         let _ = write!(&mut tmp, "{}{}", input, idx);
 
         let dig = md5::compute(&tmp);
+        Self::check_key(idx, &dig)
+    }
+
+    fn with_buffer_stretched(idx: usize, input: &str, mut tmp: &mut String) -> Option<Self> {
+        tmp.clear();
+        let _ = write!(&mut tmp, "{}{}", input, idx);
+
+        // first
+        let mut dig = md5::compute(&tmp);
+
+        for _ in 0..2016 {
+            dig = md5::compute(dig.0);
+        }
+
+        Self::check_key(idx, &dig)
+    }
+
+    fn check_key(idx: usize, dig: &md5::Digest) -> Option<Self> {
         let mut digest = [0; 32];
 
         for (from, to) in std::iter::zip(&dig.0[..], &mut digest.chunks_mut(2)) {
@@ -161,4 +250,9 @@ impl Key {
 #[test]
 fn test_one() {
     assert_eq!(22728, one_inner("abc"))
+}
+
+#[test]
+fn test_two() {
+    assert_eq!(22859, two_inner("abc"))
 }
